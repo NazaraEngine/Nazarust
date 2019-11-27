b9906d1c2f5585a44d7f0f6679f1033a2005a1df
Fomys <louis.chauvet@free.fr>
2019-11-27 21:56:58 +0100

    core: Add documentation (#2)
    * core: Add documentation
    
    Complete documentation for PixelFormatType, some values are missing because I don't found information.
    
    * core: Update tests
    
    Migrate tests to doctest

--- nazara_core/src/enums.rs
+++ nazara_core/src/enums.rs
@@ -54,11 +54,11 @@ pub enum PixelFormatType {
     ///
     /// Stored in 1 uint8
     R8,
-    ///
+    /// 8 bits red signed integral pixel
     ///
     /// Stored in 1 int8
     R8I,
-    ///
+    /// 8 bits red unsigned integral pixel
     ///
     /// Stored in 1 uint8
     R8UI,
@@ -66,27 +66,27 @@ pub enum PixelFormatType {
     ///
     /// Stored in 1 uint16
     R16,
-    ///
+    /// 16 bits red float pixel
     ///
     /// Stored in 1 half
     R16F,
-    ///
+    /// 16 bits red signed integral pixel
     ///
     /// Stored in 1 int16
     R16I,
-    ///
+    /// 16 bits red unsigned integral pixel
     ///
     /// Stored in 1 uint16
     R16UI,
-    ///
+    /// 32 bits red float pixel
     ///
     /// Stored in 1 float
     R32F,
-    ///
+    /// 32 bits red signed integral pixel
     ///
     /// Stored in 1 uint16
     R32I,
-    ///
+    /// 32 bits red unsigned integral pixel
     ///
     /// Stored in 1 uint32
     R32UI,
@@ -94,11 +94,11 @@ pub enum PixelFormatType {
     ///
     /// Stored in 2 int8
     RG8,
-    ///
+    /// 8 bits red/green signed integral pixel
     ///
     /// Stored in 2 int8
     RG8I,
-    ///
+    /// 8 bits red/green unsigned integral pixel
     ///
     /// Stored in 2 uint8
     RG8UI,
@@ -106,27 +106,27 @@ pub enum PixelFormatType {
     ///
     /// Stored in 2 uint16
     RG16,
-    ///
+    /// 16 bits red/green float pixel
     ///
     /// Stored in 2 half
     RG16F,
-    ///
+    /// 16 bits red/green signed integral pixel
     ///
     /// Stored in 2 int16
     RG16I,
-    ///
+    /// 16 bits red/green unsigned integral pixel
     ///
     /// Stored in 2 uint16
     RG16UI,
-    ///
+    /// 32 bits red/green float pixel
     ///
     /// Stored in 2 float
     RG32F,
-    ///
+    /// 32 bits red/green signed integral pixel
     ///
     /// Stored in 2 uint16
     RG32I,
-    ///
+    /// 32 bits red/green unsigned integral pixel
     ///
     /// Stored in 2 uint32
     RG32UI,   // 2*uint32
@@ -138,27 +138,27 @@ pub enum PixelFormatType {
     ///
     /// Stored in 3 uint8
     RGB8,
-    ///
+    /// 16 bits RGB pixel
     ///
     /// Stored in 3 half
     RGB16F,
-    ///
+    /// 16 bits RGB signed integral pixel
     ///
     /// Stored in 4 int16
     RGB16I,   // 4*int16
-    ///
+    /// 16 bits RGB unsigned integral pixel
     ///
     /// Stored in 4 uint16
     RGB16UI,
-    ///
+    /// 32 bits RGB pixel
     ///
     /// Stored in 3 float
     RGB32F,
-    ///
+    /// 32 bits RGB signed integral pixel
     ///
     /// Stored in 4 int 32
     RGB32I,
-    ///
+    /// 32 bits RGB unsigned integral pixel
     ///
     /// Stored in 4 uint32
     RGB32UI,
@@ -170,35 +170,35 @@ pub enum PixelFormatType {
     ///
     /// Stored in 4 uint8
     RGBA8,
-    ///
+    /// 16 bits RGBA float pixel
     ///
     /// Stored in 4 half
     RGBA16F,
-    ///
+    /// 16 bits RGBA signed integral pixel
     ///
     /// Stored in 4 int16
     RGBA16I,
-    ///
+    /// 16 bits RGBA unsigned integral
     ///
     /// Stored in 4 uint16
     RGBA16UI,
-    ///
+    /// 32 bits RGBA float pixel
     ///
     /// Stored in 4 floats
     RGBA32F,
-    ///
+    /// 32 bits RGBA signed integral pixel
     ///
     /// Stored in 4 int32
     RGBA32I,
-    ///
+    /// 32 bits RGBA unsigned integral pixel
     ///
     /// Stored in 4 uint32
     RGBA32UI,
-    ///
+    /// 16 bits depth floating pixel
     ///
     /// Stored in
     Depth16,
-    ///
+    /// 24 bits depth floating pixel
     ///
     /// Stored in
     Depth24,
@@ -206,7 +206,7 @@ pub enum PixelFormatType {
     ///
     /// Stored in
     Depth24Stencil8,
-    ///
+    /// 32 bits depth floating pixel
     ///
     /// Stored in
     Depth32,
--- nazara_core/src/image.rs
+++ nazara_core/src/image.rs
@@ -18,9 +18,13 @@ impl Image {
     ///
     /// ```
     /// use nazara_core::image::Image;
-    /// use nazara_core::enums::PixelFormatType;
+    /// use nazara_core::enums::{PixelFormatType, ImageType};
+    /// use cgmath::Vector3;
     ///
     /// let image = Image::new_1d(PixelFormatType::A8, 40);
+    /// assert_eq!(image.get_size(), Vector3 { x: 40, y: 1, z: 1 });
+    /// assert_eq!(image.get_pixel_format(), PixelFormatType::A8);
+    /// assert_eq!(image.get_image_type(), ImageType::Single1D);
     /// ```
     ///
     /// # Arguments
@@ -44,9 +48,13 @@ impl Image {
     ///
     /// ```
     /// use nazara_core::image::Image;
-    /// use nazara_core::enums::PixelFormatType;
+    /// use nazara_core::enums::{PixelFormatType, ImageType};
+    /// use cgmath::Vector3;
     ///
     /// let image = Image::new_2d(PixelFormatType::A8, 40, 50);
+    /// assert_eq!(image.get_size(), Vector3 { x: 40, y: 50, z: 1 });
+    /// assert_eq!(image.get_pixel_format(), PixelFormatType::A8);
+    /// assert_eq!(image.get_image_type(), ImageType::Single2D);
     /// ```
     ///
     /// # Arguments
@@ -70,9 +78,13 @@ impl Image {
     ///
     /// ```
     /// use nazara_core::image::Image;
-    /// use nazara_core::enums::PixelFormatType;
+    /// use nazara_core::enums::{PixelFormatType, ImageType};
+    /// use cgmath::Vector3;
     ///
     /// let image = Image::new_3d(PixelFormatType::A8, 40, 50, 60);
+    /// assert_eq!(image.get_size(), Vector3 { x: 40, y: 50, z: 60 });
+    /// assert_eq!(image.get_pixel_format(), PixelFormatType::A8);
+    /// assert_eq!(image.get_image_type(), ImageType::Single3D);
     /// ```
     ///
     /// # Arguments
@@ -95,7 +107,7 @@ impl Image {
     /// You can get image type from any [`Image`] instance:
     /// ```
     /// use nazara_core::image::Image;
-    /// use nazara_core::enums::PixelFormatType;
+    /// use nazara_core::enums::{PixelFormatType, ImageType};
     ///
     /// let img_1d = Image::new_1d(PixelFormatType::A8, 40);
     /// let img_2d = Image::new_2d(PixelFormatType::A8, 40, 50);
@@ -103,6 +115,10 @@ impl Image {
     /// let img_1d_type = img_1d.get_image_type(); // img_1d_type is ImageType::Single1D
     /// let img_2d_type = img_2d.get_image_type(); // img_2d_type is ImageType::Single2D
     /// let img_3d_type = img_3d.get_image_type(); // img_3d_type is ImageType::Single3D
+    ///
+    /// assert_eq!(img_1d_type, ImageType::Single1D);
+    /// assert_eq!(img_2d_type, ImageType::Single2D);
+    /// assert_eq!(img_3d_type, ImageType::Single3D);
     /// ```
     pub fn get_image_type(&self) -> ImageType {
         self.image_type
@@ -118,9 +134,13 @@ impl Image {
     /// let img_1d = Image::new_1d(PixelFormatType::A8, 40);
     /// let img_2d = Image::new_2d(PixelFormatType::R16F, 40, 50);
     /// let img_3d = Image::new_3d(PixelFormatType::RGB16F, 40, 50, 60);
-    /// let img_1d_pixel_type = img_1d.get_image_type(); // img_1d_type is PixelFormatType::A8
-    /// let img_2d_pixel_type = img_2d.get_image_type(); // img_2d_type is PixelFormatType::R16F
-    /// let img_3d_pixel_type = img_3d.get_image_type(); // img_3d_type is PixelFormatType::RGB16F
+    /// let img_1d_pixel_type = img_1d.get_pixel_format(); // img_1d_type is PixelFormatType::A8
+    /// let img_2d_pixel_type = img_2d.get_pixel_format(); // img_2d_type is PixelFormatType::R16F
+    /// let img_3d_pixel_type = img_3d.get_pixel_format(); // img_3d_type is PixelFormatType::RGB16F
+    ///
+    /// assert_eq!(img_1d_pixel_type, PixelFormatType::A8);
+    /// assert_eq!(img_2d_pixel_type, PixelFormatType::R16F);
+    /// assert_eq!(img_3d_pixel_type, PixelFormatType::RGB16F);
     /// ```
     pub fn get_pixel_format(&self) -> PixelFormatType {
         self.pixel_format
@@ -132,6 +152,7 @@ impl Image {
     /// ```
     /// use nazara_core::image::Image;
     /// use nazara_core::enums::PixelFormatType;
+    /// use cgmath::Vector3;
     ///
     /// let img_1d = Image::new_1d(PixelFormatType::A8, 40);
     /// let img_2d = Image::new_2d(PixelFormatType::R16F, 40, 50);
@@ -139,39 +160,12 @@ impl Image {
     /// let img_1d_size = img_1d.get_size(); // img_1d_size is Vector3 { x: 40, y: 1, z: 1 }
     /// let img_2d_size = img_2d.get_size(); // img_2d_size is Vector3 { x: 40, y: 50, z: 1 }
     /// let img_3d_size = img_3d.get_size(); // img_3d_size is Vector3 { x: 40, y: 50, z: 60 }
+    ///
+    /// assert_eq!(img_1d_size, Vector3 { x: 40, y: 1, z: 1 });
+    /// assert_eq!(img_2d_size, Vector3 { x: 40, y: 50, z: 1 });
+    /// assert_eq!(img_3d_size, Vector3 { x: 40, y: 50, z: 60 });
     /// ```
     pub fn get_size(&self) -> Vector3<usize> {
         self.dimensions
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::image::Image;
-    use crate::enums::{PixelFormatType, ImageType};
-    use cgmath::Vector3;
-
-    #[test]
-    fn test_new_1d () {
-        let new_1d = Image::new_1d(PixelFormatType::A8, 50);
-        assert_eq!(new_1d.get_size(), Vector3 { x: 50, y: 1, z: 1 });
-        assert_eq!(new_1d.get_pixel_format(), PixelFormatType::A8);
-        assert_eq!(new_1d.get_image_type(), ImageType::Single1D);
-    }
-
-    #[test]
-    fn test_new_2d () {
-        let new_2d = Image::new_2d(PixelFormatType::A8, 50, 60);
-        assert_eq!(new_2d.get_size(), Vector3 { x: 50, y: 60, z: 1 });
-        assert_eq!(new_2d.get_pixel_format(), PixelFormatType::A8);
-        assert_eq!(new_2d.get_image_type(), ImageType::Single2D);
-    }
-
-    #[test]
-    fn test_new_3d () {
-        let new_3d = Image::new_3d(PixelFormatType::A8, 50, 60, 70);
-        assert_eq!(new_3d.get_size(), Vector3 { x: 50, y: 60, z: 70 });
-        assert_eq!(new_3d.get_pixel_format(), PixelFormatType::A8);
-        assert_eq!(new_3d.get_image_type(), ImageType::Single3D);
     }
 }
