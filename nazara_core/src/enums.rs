6227ad3c018c78019aa644d6a84236079bf9aade
Fomys <fomys@gmx.us>
2019-11-27 09:20:01 +0100

    Ajout des tests (#1)
    * core: add tests for Image::new_{1d,2d,3d}
    
    * core: add documentation
    
    Add documentation for Image struct and its implementation.
    Add documentation for ImageType Enum.
    Add documentation for PixelFormatType Enum, check if https://docs.rs/luminance/0.37.1/luminance/pixel/index.html could be a good solution.

--- nazara_core/src/enums.rs
+++ nazara_core/src/enums.rs
@@ -1,65 +1,229 @@
-#[derive(Clone, Copy)]
+#[derive(Debug, PartialEq, Clone, Copy)]
+/// Represents an image format
 pub enum ImageType {
+    /// TODO
     Array1D,
+    /// TODO
     Array2D,
+    /// TODO
     Cubemap,
+    /// 1 dimensional image type
     Single1D,
+    /// 2 dimensional image type
     Single2D,
+    /// 3 dimensional image type
     Single3D,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, PartialEq, Clone, Copy)]
+/// Represents a pixel format type.
 pub enum PixelFormatType {
-    A8,    // 1*uint8
-    BGR8,  // 3*uint8
-    BGRA8, // 4*uint8
+    /// 8 bits grey pixel
+    ///
+    /// Stored in 1 uint8
+    A8,
+    /// 24 bits BGR pixel
+    ///
+    /// Stored in 3 uint8
+    BGR8,
+    /// 32 bits BGRA pixel
+    ///
+    /// Stored in 4 uint8
+    BGRA8,
+    ///
+    ///
+    /// Stored in
     DXT1,
+    ///
+    ///
+    /// Stored in
     DXT3,
+    ///
+    ///
+    /// Stored in
     DXT5,
-    L8,       // 1*uint8
-    LA8,      // 2*uint8
-    R8,       // 1*uint8
-    R8I,      // 1*int8
-    R8UI,     // 1*uint8
-    R16,      // 1*uint16
-    R16F,     // 1*half
-    R16I,     // 1*int16
-    R16UI,    // 1*uint16
-    R32F,     // 1*float
-    R32I,     // 1*uint16
-    R32UI,    // 1*uint32
-    RG8,      // 2*int8
-    RG8I,     // 2*int8
-    RG8UI,    // 2*uint8
-    RG16,     // 2*uint16
-    RG16F,    // 2*half
-    RG16I,    // 2*int16
-    RG16UI,   // 2*uint16
-    RG32F,    // 2*float
-    RG32I,    // 2*uint16
+    /// 8 bits luminance pixel
+    ///
+    /// Stored in 1 uint8
+    L8,
+    /// 16 bits luminance with alpha pixel
+    ///
+    /// Stored in 2 uint8
+    LA8,
+    /// 8 bits red only pixel
+    ///
+    /// Stored in 1 uint8
+    R8,
+    ///
+    ///
+    /// Stored in 1 int8
+    R8I,
+    ///
+    ///
+    /// Stored in 1 uint8
+    R8UI,
+    /// 16 bits red only pixel
+    ///
+    /// Stored in 1 uint16
+    R16,
+    ///
+    ///
+    /// Stored in 1 half
+    R16F,
+    ///
+    ///
+    /// Stored in 1 int16
+    R16I,
+    ///
+    ///
+    /// Stored in 1 uint16
+    R16UI,
+    ///
+    ///
+    /// Stored in 1 float
+    R32F,
+    ///
+    ///
+    /// Stored in 1 uint16
+    R32I,
+    ///
+    ///
+    /// Stored in 1 uint32
+    R32UI,
+    /// 16 bits red/green pixel
+    ///
+    /// Stored in 2 int8
+    RG8,
+    ///
+    ///
+    /// Stored in 2 int8
+    RG8I,
+    ///
+    ///
+    /// Stored in 2 uint8
+    RG8UI,
+    /// 32 bits red/green pixel
+    ///
+    /// Stored in 2 uint16
+    RG16,
+    ///
+    ///
+    /// Stored in 2 half
+    RG16F,
+    ///
+    ///
+    /// Stored in 2 int16
+    RG16I,
+    ///
+    ///
+    /// Stored in 2 uint16
+    RG16UI,
+    ///
+    ///
+    /// Stored in 2 float
+    RG32F,
+    ///
+    ///
+    /// Stored in 2 uint16
+    RG32I,
+    ///
+    ///
+    /// Stored in 2 uint32
     RG32UI,   // 2*uint32
-    RGB5A1,   // 3*uint5 + alpha bit
-    RGB8,     // 3*uint8
-    RGB16F,   // 3*half
+    /// 16-bits RGBA pixel
+    ///
+    /// Stored in 3 uint5 and 1 alpha bit
+    RGB5A1,
+    /// 24 bits RGB pixel
+    ///
+    /// Stored in 3 uint8
+    RGB8,
+    ///
+    ///
+    /// Stored in 3 half
+    RGB16F,
+    ///
+    ///
+    /// Stored in 4 int16
     RGB16I,   // 4*int16
-    RGB16UI,  // 4*uint16
-    RGB32F,   // 3*float
-    RGB32I,   // 4*int32
-    RGB32UI,  // 4*uint32
-    RGBA4,    // 4*uint4
-    RGBA8,    // 4*uint8
-    RGBA16F,  // 4*half
-    RGBA16I,  // 4*int16
-    RGBA16UI, // 4*uint16
-    RGBA32F,  // 4*float
-    RGBA32I,  // 4*int32
-    RGBA32UI, // 4*uint32
+    ///
+    ///
+    /// Stored in 4 uint16
+    RGB16UI,
+    ///
+    ///
+    /// Stored in 3 float
+    RGB32F,
+    ///
+    ///
+    /// Stored in 4 int 32
+    RGB32I,
+    ///
+    ///
+    /// Stored in 4 uint32
+    RGB32UI,
+    ///
+    ///
+    /// Stored in 4 uint4
+    RGBA4,
+    /// 32 bits RGBA pixel
+    ///
+    /// Stored in 4 uint8
+    RGBA8,
+    ///
+    ///
+    /// Stored in 4 half
+    RGBA16F,
+    ///
+    ///
+    /// Stored in 4 int16
+    RGBA16I,
+    ///
+    ///
+    /// Stored in 4 uint16
+    RGBA16UI,
+    ///
+    ///
+    /// Stored in 4 floats
+    RGBA32F,
+    ///
+    ///
+    /// Stored in 4 int32
+    RGBA32I,
+    ///
+    ///
+    /// Stored in 4 uint32
+    RGBA32UI,
+    ///
+    ///
+    /// Stored in
     Depth16,
+    ///
+    ///
+    /// Stored in
     Depth24,
+    ///
+    ///
+    /// Stored in
     Depth24Stencil8,
+    ///
+    ///
+    /// Stored in
     Depth32,
+    ///
+    ///
+    /// Stored in
     Stencil1,
+    ///
+    ///
+    /// Stored in
     Stencil4,
+    ///
+    ///
+    /// Stored in
     Stencil8,
+    ///
+    ///
+    /// Stored in
     Stencil16,
 }
--- nazara_core/src/image.rs
+++ nazara_core/src/image.rs
@@ -1,6 +1,9 @@
 use crate::enums::{ImageType, PixelFormatType};
 use cgmath::Vector3;
 
+/// Image structure for Nazarust
+///
+/// Mettre ici une description plus avancé, j'ai pas encore compris à quoi ça sert
 pub struct Image {
     dimensions: Vector3<usize>,
     content: Vec<Vec<u8>>,
@@ -9,6 +12,23 @@ pub struct Image {
 }
 
 impl Image {
+    /// Creates a new [`ImageType::Single1D`] [`Image`] with the given pixel format and width.
+    ///
+    /// You can create a new [`ImageType::Single1D`] image:
+    ///
+    /// ```
+    /// use nazara_core::image::Image;
+    /// use nazara_core::enums::PixelFormatType;
+    ///
+    /// let image = Image::new_1d(PixelFormatType::A8, 40);
+    /// ```
+    ///
+    /// # Arguments
+    ///
+    /// * `format` - Format for stored pixels
+    /// * `width` - Width for the new image
+    ///
+    /// [`ImageType::Single1D`]: crate::enums::ImageType::Single1D
     pub fn new_1d(format: PixelFormatType, width: usize) -> Image {
         Image {
             dimensions: Vector3::new(width, 1, 1),
@@ -18,6 +38,23 @@ impl Image {
         }
     }
 
+    /// Creates a new [`ImageType::Single2D`] [`Image`] with the given pixel format, width and height.
+    ///
+    /// You can create a new [`ImageType::Single2D`] image:
+    ///
+    /// ```
+    /// use nazara_core::image::Image;
+    /// use nazara_core::enums::PixelFormatType;
+    ///
+    /// let image = Image::new_2d(PixelFormatType::A8, 40, 50);
+    /// ```
+    ///
+    /// # Arguments
+    ///
+    /// * `format` - Format for stored pixels
+    /// * `width` - Width for the new image
+    ///
+    /// [`ImageType::Single2D`]: crate::enums::ImageType::Single2D
     pub fn new_2d(format: PixelFormatType, width: usize, height: usize) -> Image {
         Image {
             dimensions: Vector3::new(width, height, 1),
@@ -27,6 +64,23 @@ impl Image {
         }
     }
 
+    /// Creates a new [`ImageType::Single3D`] [`Image`] with the given pixel format, width, height and depth.
+    ///
+    /// You can create a new [`ImageType::Single3D`] image:
+    ///
+    /// ```
+    /// use nazara_core::image::Image;
+    /// use nazara_core::enums::PixelFormatType;
+    ///
+    /// let image = Image::new_3d(PixelFormatType::A8, 40, 50, 60);
+    /// ```
+    ///
+    /// # Arguments
+    ///
+    /// * `format` - Format for stored pixels
+    /// * `width` - Width for the new image
+    ///
+    /// [`ImageType::Single3D`]: crate::enums::ImageType::Single3D
     pub fn new_3d(format: PixelFormatType, width: usize, height: usize, depth: usize) -> Image {
         Image {
             dimensions: Vector3::new(width, height, depth),
@@ -36,15 +90,88 @@ impl Image {
         }
     }
 
+    /// Return image type of an [`Image`] instance.
+    ///
+    /// You can get image type from any [`Image`] instance:
+    /// ```
+    /// use nazara_core::image::Image;
+    /// use nazara_core::enums::PixelFormatType;
+    ///
+    /// let img_1d = Image::new_1d(PixelFormatType::A8, 40);
+    /// let img_2d = Image::new_2d(PixelFormatType::A8, 40, 50);
+    /// let img_3d = Image::new_3d(PixelFormatType::A8, 40, 50, 60);
+    /// let img_1d_type = img_1d.get_image_type(); // img_1d_type is ImageType::Single1D
+    /// let img_2d_type = img_2d.get_image_type(); // img_2d_type is ImageType::Single2D
+    /// let img_3d_type = img_3d.get_image_type(); // img_3d_type is ImageType::Single3D
+    /// ```
     pub fn get_image_type(&self) -> ImageType {
         self.image_type
     }
 
+    /// Return pixel format of an [`Image`] instance.
+    ///
+    /// You can get pixel type from any [`Image`] instance:
+    /// ```
+    /// use nazara_core::image::Image;
+    /// use nazara_core::enums::PixelFormatType;
+    ///
+    /// let img_1d = Image::new_1d(PixelFormatType::A8, 40);
+    /// let img_2d = Image::new_2d(PixelFormatType::R16F, 40, 50);
+    /// let img_3d = Image::new_3d(PixelFormatType::RGB16F, 40, 50, 60);
+    /// let img_1d_pixel_type = img_1d.get_image_type(); // img_1d_type is PixelFormatType::A8
+    /// let img_2d_pixel_type = img_2d.get_image_type(); // img_2d_type is PixelFormatType::R16F
+    /// let img_3d_pixel_type = img_3d.get_image_type(); // img_3d_type is PixelFormatType::RGB16F
+    /// ```
     pub fn get_pixel_format(&self) -> PixelFormatType {
         self.pixel_format
     }
 
+    /// Return size of an [`Image`] instance.
+    ///
+    /// You can get size from any [`Image`] instance:
+    /// ```
+    /// use nazara_core::image::Image;
+    /// use nazara_core::enums::PixelFormatType;
+    ///
+    /// let img_1d = Image::new_1d(PixelFormatType::A8, 40);
+    /// let img_2d = Image::new_2d(PixelFormatType::R16F, 40, 50);
+    /// let img_3d = Image::new_3d(PixelFormatType::RGB16F, 40, 50, 60);
+    /// let img_1d_size = img_1d.get_size(); // img_1d_size is Vector3 { x: 40, y: 1, z: 1 }
+    /// let img_2d_size = img_2d.get_size(); // img_2d_size is Vector3 { x: 40, y: 50, z: 1 }
+    /// let img_3d_size = img_3d.get_size(); // img_3d_size is Vector3 { x: 40, y: 50, z: 60 }
+    /// ```
     pub fn get_size(&self) -> Vector3<usize> {
         self.dimensions
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::image::Image;
+    use crate::enums::{PixelFormatType, ImageType};
+    use cgmath::Vector3;
+
+    #[test]
+    fn test_new_1d () {
+        let new_1d = Image::new_1d(PixelFormatType::A8, 50);
+        assert_eq!(new_1d.get_size(), Vector3 { x: 50, y: 1, z: 1 });
+        assert_eq!(new_1d.get_pixel_format(), PixelFormatType::A8);
+        assert_eq!(new_1d.get_image_type(), ImageType::Single1D);
+    }
+
+    #[test]
+    fn test_new_2d () {
+        let new_2d = Image::new_2d(PixelFormatType::A8, 50, 60);
+        assert_eq!(new_2d.get_size(), Vector3 { x: 50, y: 60, z: 1 });
+        assert_eq!(new_2d.get_pixel_format(), PixelFormatType::A8);
+        assert_eq!(new_2d.get_image_type(), ImageType::Single2D);
+    }
+
+    #[test]
+    fn test_new_3d () {
+        let new_3d = Image::new_3d(PixelFormatType::A8, 50, 60, 70);
+        assert_eq!(new_3d.get_size(), Vector3 { x: 50, y: 60, z: 70 });
+        assert_eq!(new_3d.get_pixel_format(), PixelFormatType::A8);
+        assert_eq!(new_3d.get_image_type(), ImageType::Single3D);
     }
 }
