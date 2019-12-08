fc89967d9e120bf1b72585a6e19bb604c659b2bd
Faymoon <mercier.mateo@orange.fr>
2019-12-08 11:07:23 +0100

    Work in progress on RigidBody

--- nazara_physics2d/src/rigidbody.rs
+++ nazara_physics2d/src/rigidbody.rs
@@ -1,22 +1,48 @@
 use nalgebra::{Vector2, Point2, RealField};
-use nphysics2d::object::{BodyStatus, RigidBodyDesc, DefaultBodyHandle};
+use nphysics2d::object::{RigidBodyDesc, ColliderDesc, DefaultBodyHandle, BodyPartHandle, DefaultColliderHandle};
+use nphysics2d::object;
 use nphysics2d::math::{Velocity, Inertia};
 
 use crate::physworld::PhysWorld;
+use crate::collider::Collider;
 
-pub struct RigidBody
+pub struct RigidBody<'a, T: RealField>
 {
-    handle: DefaultBodyHandle,
+    body: &'a mut object::RigidBody<T>,
+    //collider_handles: Vec<DefaultColliderHandle>,
 }
 
-impl RigidBody
+impl<'a, T: RealField> RigidBody<'a, T>
 {
-    pub fn new<T: RealField>(mut world: PhysWorld<T>) -> RigidBody
+    pub fn new(world: &'a mut PhysWorld<T>, mass: T, collider: Option<&Collider<T>>) -> Self
     {
-        let body = RigidBodyDesc::new().build();
+        let handle = world.body_set.insert(RigidBodyDesc::new().mass(mass).build());
+
+        if let Some(c) = collider
+        {
+            world.collider_set.insert(c.create_desc().build(BodyPartHandle(handle, 0)));
+        }
+
         RigidBody
         {
-            handle: world.body_set.insert(body),
+            body: world.body_set.rigid_body_mut(handle).unwrap(),
+            /*collider_handles: 
+                match collider
+                {
+                    Some(c) => vec![world.collider_set.insert(c.create_desc().build(BodyPartHandle(handle, 0)))],
+                    None => Vec::new(),
+                },*/
         }
     }
+
+    pub fn get_position(&self) -> Vector2<T>
+    {
+        self.body.position().translation.vector
+    }
+
+    /*pub fn add_collider(&mut self, collider: &Collider<T>)
+    {
+        let collider = collider.create_desc().build(BodyPartHandle(self.handle, 0));
+        self.collider_handles.push(self.world.collider_set.insert(collider));
+    }*/
 }
