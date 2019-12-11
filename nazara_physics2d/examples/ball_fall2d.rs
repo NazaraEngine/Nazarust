91b425d986e3f640de370d295fa267d700c5e771
Faymoon <mercier.mateo@orange.fr>
2019-12-11 17:27:40 +0100

    Update example to support material

--- nazara_physics2d/examples/ball_fall2d.rs
+++ nazara_physics2d/examples/ball_fall2d.rs
@@ -8,6 +8,7 @@ use nazara_physics2d::physworld::PhysWorld;
 use nazara_physics2d::rigidbody::RigidBody;
 use nazara_physics2d::rigidbody::RigidBodyStatus;
 use nazara_physics2d::collider::Collider;
+use nazara_physics2d::material::Material;
 
 use nalgebra::Vector2;
 use nalgebra::Point2;
@@ -16,11 +17,12 @@ fn main()
 {
     let mut world = PhysWorld::new(Vector2::y() * -9.81);
 
+    let material = Material::new(0.1, 0.0, None);
     let collider = Collider::Circle{ radius: 4.0, offset: None };
-    let body = RigidBody::new(&mut world, 1.0, RigidBodyStatus::Dynamic, Some(&collider));
+    let body = RigidBody::new(&mut world, 1.0, Some(&collider), Some(&material));
 
     let ground_collider = Collider::Box{ size: Vector2::new(20.0, 5.0), offset: Some(Point2::new(-10.0, -30.0)) };
-    let ground = RigidBody::new(&mut world, 0.0, RigidBodyStatus::Static, Some(&ground_collider)); // mass is useles for static bodies 
+    let ground = RigidBody::new_static(&mut world, Some(&ground_collider), None); // mass is useles for static bodies 
 
     let mut instant = Instant::now();
     loop
