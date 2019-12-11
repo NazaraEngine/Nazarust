867ed8654c21168e12b576467a4187bcca41a13d
Faymoon <mercier.mateo@orange.fr>
2019-12-11 15:09:44 +0100

    Update API

--- nazara_physics2d/examples/ball_fall2d.rs
+++ nazara_physics2d/examples/ball_fall2d.rs
@@ -6,14 +6,17 @@ use nazara_physics2d::physworld::PhysWorld;
 use nazara_physics2d::rigidbody::RigidBody;
 use nazara_physics2d::collider::Collider;
 use nalgebra::Vector2;
+use nalgebra::Point2;
 
 fn main()
 {
     let mut world = PhysWorld::new(Vector2::y() * -9.81);
 
-    let collider = Collider::Circle(4.0, None);
+    let collider = Collider::Circle{ radius: 4.0, offset: None };
     let body = RigidBody::new(&mut world, 1.0, Some(&collider));
 
+    let ground_collider = Collider::Box{ size: Vector2::new(20.0, 5.0), offset: Some(Point2::new(-10.0, 30.0)) };
+    let ground = RigidBody::new(&mut world, 0.0, Some(&ground_collider));
     loop
     {
         world.step(0.5);
--- nazara_physics2d/src/collider.rs
+++ nazara_physics2d/src/collider.rs
@@ -5,12 +5,12 @@ use ncollide2d::shape;
 
 pub enum Collider<T: RealField>
 {
-    //Box(Vector2<T> /*size*/, Option<Point2<T>> /*offset*/), // TODO: understand cuboïd collider
-    Circle(T /*radius*/, Option<Point2<T>> /*offset*/),
-    Capsule(T /*half_height*/, T /*radius*/),
-    //Compound(Vec<Collider<T>> /*colliders*/),
-    Segment(Point2<T> /*first_point*/, Point2<T> /*second_point*/),
-    //Triangle(Point2<T> /*first_point*/, Point2<T> /*second_point*/, Point2<T> /*third_point*/), because shape::Triangle doesn't impl shape trait
+    Box{ size: Vector2<T>, offset: Option<Point2<T>> },
+    Circle{ radius: T, offset: Option<Point2<T>> },
+    Capsule{ radius: T, half_height: T },
+    //Compound(colliders: Vec<Collider<T>> ),
+    Segment{ first_point: Point2<T> , second_point: Point2<T> },
+    //Triangle(first_point: Point2<T>, second_point: Point2<T>, third_point: Point2<T>), because shape::Triangle doesn't impl shape trait for no reason
 }
 
 impl<T: RealField> Collider<T>
@@ -19,7 +19,16 @@ impl<T: RealField> Collider<T>
     {
         match self
         {
-            &Collider::Circle(radius, offset) => 
+            &Collider::Box{ size, offset } =>
+            {
+                let mut desc = ColliderDesc::new(shape::ShapeHandle::new(shape::Cuboid::new(size.scale(T::two_pi()/T::pi())))); // *2 because Cuboid expect half size
+                if let Some(offset) = offset
+                {
+                    desc.set_translation(offset.coords);
+                }
+                desc
+            }
+            &Collider::Circle{ radius, offset } => 
             {
                 let mut desc = ColliderDesc::new(shape::ShapeHandle::new(shape::Ball::new(radius)));
                 if let Some(offset) = offset
@@ -28,10 +37,10 @@ impl<T: RealField> Collider<T>
                 }
                 desc
             },
-            &Collider::Capsule(half_height, radius) =>
+            &Collider::Capsule{ radius, half_height } =>
                 ColliderDesc::new(shape::ShapeHandle::new(shape::Capsule::new(half_height, radius))),
             //&Compound(colliders) =>
-            &Collider::Segment(first_point, second_point) =>
+            &Collider::Segment{ first_point, second_point } =>
                 ColliderDesc::new(shape::ShapeHandle::new(shape::Segment::new(first_point, second_point))),
             //&Triangle(first_point, second_point, third_point) =>
             //    ColliderDesc::new(shape::ShapeHandle::new(shape::Triangle::new(first_point, second_point, third_point))),
